614d52ac6de4d1600a79259f16c2a050aeebece9
Vyacheslav Tereshchenko <vyacheslav.tereshchenko@yahoo.com>
2025-04-10 23:30:56 +0200

    Add initial configuration

---
+++ .storybook/main.ts
@@ -0,0 +1,24 @@
+import type { StorybookConfig } from "@storybook/react-vite";
+
+const config: StorybookConfig = {
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  addons: [
+    "@storybook/addon-essentials",
+    "@storybook/addon-onboarding",
+    "@chromatic-com/storybook",
+    "@storybook/experimental-addon-test",
+    "@storybook/addon-themes",
+    "@storybook/addon-docs",
+  ],
+  framework: {
+    name: "@storybook/react-vite",
+    options: {},
+  },
+
+  docs: {},
+
+  typescript: {
+    reactDocgen: "react-docgen-typescript",
+  },
+};
+export default config;
